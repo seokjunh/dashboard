b03b06a9f9f294c237415a91b9356c5868bbad2b
seokjun <seokjunh97@gmail.com>
2024-05-23 15:22:12 +0900

    회원가입,로그인,로그아웃 코드 구현

---
+++ Assets/Script/FirebaseAuthManager.cs
@@ -0,0 +1,63 @@
+using Firebase.Auth;
+using TMPro;
+using UnityEngine;
+
+public class FirebaseAuthManager : MonoBehaviour
+{
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    public TMP_InputField email;
+    public TMP_InputField password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    public void SignUp()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("회원가입 취소");
+            }
+            else if (task.IsFaulted)
+            {
+                Debug.LogError("회원가입 실패");
+            }
+            else if (task.IsCompleted)
+            {
+                Debug.LogFormat("회원가입 성공");
+                AuthResult newUser = task.Result;
+            }
+        });
+    }
+
+    public void SignIn()
+    {
+        auth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("로그인 취소");
+            }
+            else if (task.IsFaulted)
+            {
+                Debug.LogError("로그인 실패");
+            }
+            else if (task.IsCompleted)
+            {
+                Debug.LogFormat("로그인 성공");
+                AuthResult newUser = task.Result;
+            }
+        });
+    }
+
+    public void SignOut()
+    {
+        auth.SignOut();
+    }
+
+}
